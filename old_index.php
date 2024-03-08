a66b7a6f8504c83e55beb7c1ce3bb8c6aa502240
nosisatlas <fz.farhane109@gmail.com>
2024-03-08 14:07:27 +0100

    Setting up the pagination - Need to fix display of another page when there's no more post

--- old_index.php
+++ old_index.php
@@ -14,29 +14,10 @@
                     Page Heading
                     <small>Secondary Text</small>
                 </h1>
+                <!-- Displaying posts from Db -->
                 <?php 
-                    // Limiting the posts in the page
-                    $per_page = 5;
-                    if(isset($_GET['page'])){
-                        $page = $_GET['page'];
-                    }else{
-                        $page = "";
-                    }
-                    if($page == "" || $page == 1){
-                        $page_1 = 0;
-                    }else{
-                        $page_1 = ($page * $per_page) - $per_page;
-                    }
-                    // Displayin posts in pagination
-                    $post_query_count = "SELECT * FROM posts WHERE post_status = 'published'";
-                    $find_count = mysqli_query($connection, $query);
-                    $post_count = mysqli_num_rows($find_count);
-                    $post_count = ceil($post_count / $per_page);
-
-                    // Displayin posts from db
-                    $post_query_count = "SELECT * FROM posts WHERE post_status = 'published' LIMIT $page_1, $per_page";
-                    $select_all_posts_query = mysqli_query($connection, $post_query_count);
-                    checkQuery($select_all_posts_query);
+                    $query = "SELECT * FROM posts WHERE post_status = 'published'";
+                    $select_all_posts_query = mysqli_query($connection, $query);
                     if(mysqli_num_rows($select_all_posts_query) > 0) {
                         while($row = mysqli_fetch_assoc($select_all_posts_query)){
                             $post_id = $row['post_id'];
@@ -85,18 +66,5 @@
         <!-- /.row -->
 
         <hr>
-
-        <ul class="pager">
-            <?php 
-                // Loop for displaying pagination
-                for($i = 1; $i <= $post_count; $i++ )
-                if($i == $page){
-                    echo "<li><a class='active_link' href='index.php?page={$i}'>{$i}</a></li>";
-                }else{
-                    echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
-                }
-            
-            ?>
-        </ul>
         
 <?php include 'includes/footer.php' ?>
