0573701814a72e6aa7537bcb0b280cf0af1e7370
nosisatlas <fz.farhane109@gmail.com>
2024-03-08 16:54:45 +0100

    Creating the users online table and setup the code in admin/index

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,21 @@
 <?php include 'includes/admin_header.php' ?>
 
     <div id="wrapper">
-
+        <?php 
+            $session = session_id();
+            $time = time();
+            $time_out_in_seconds = 60;
+            $time_out = $time - $time_out_in_seconds;
+            $query = "SELECT * FROM users_online WHERE session = '$session'";
+            $send_online_users_query = mysqli_query($connection, $query);
+            checkQuery($send_online_users_query);
+            $count = mysqli_num_rows($send_online_users_query);
+            if($count == NULL){
+                mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('$session','$time')");
+            }else{
+                mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('$session','$time')");
+            }
+        ?>
         <!-- Navigation -->
         <?php include 'includes/admin_navigation.php' ?>
 
