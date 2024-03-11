be94fea99491b6e3f89a01bcd4cf8b29425035cb
nosisatlas <fz.farhane109@gmail.com>
2024-03-11 22:14:19 +0100

    Installing phpmailer - Connecting to mailtrap and sending test email

--- forgot.php
+++ forgot.php
@@ -1,3 +1,50 @@
+<?php 
+    // Sending emails for forgot pass
+    //Import PHPMailer classes into the global namespace
+    //These must be at the top of your script, not inside a function
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+    //Load Composer's autoloader
+    require 'vendor/autoload.php';
+
+    //Create an instance; passing `true` enables exceptions
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'sandbox.smtp.mailtrap.io';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'e8b8dfc05dae4f';                     //SMTP username
+        $mail->Password   = 'f116d7a93bb9d5';                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port       = 587;                                    
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+
+        //Recipients
+        $mail->setFrom('nosis@atlas.com', 'Mailer');
+        $mail->addAddress('support@cms.com', 'Admin');     //Add a recipient
+        $mail->addAddress('ellen@example.com');               //Name is optional
+        $mail->addReplyTo('info@example.com', 'Information');
+        $mail->addCC('cc@example.com');
+        $mail->addBCC('bcc@example.com');
+
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'Here is the subject';
+        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+?>
+
 <?php  include "includes/header.php"; ?>
 
 <?php 
