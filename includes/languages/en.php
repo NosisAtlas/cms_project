1c9d234f7887ce6ae100b04b77d91b6472ba9527
nosisatlas <fz.farhane109@gmail.com>
2024-03-13 18:19:22 +0100

    Adding consts in languages en file

---
+++ includes/languages/en.php
@@ -0,0 +1,7 @@
+<?php 
+    // Defining consts
+    const _REGISTER = "Register";
+    const _USERNAME = "Enter your desired username";
+    const _EMAIL = "username@mail.com";
+    const _PASSWORD = "Ente your desired password";
+?>
