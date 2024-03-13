02a8c76164d3692864e9d1a2ac2d4e0849178ea6
nosisatlas <fz.farhane109@gmail.com>
2024-03-13 18:21:58 +0100

    Adding consts in languages kr file

---
+++ includes/languages/kr.php
@@ -0,0 +1,7 @@
+<?php 
+    // Defining consts
+    const _REGISTER = "등록";
+    const _USERNAME = "원하는 사용자 이름을 입력하세요";
+    const _EMAIL = "username@mail.com";
+    const _PASSWORD = "원하는 비밀번호를 입력하세요";
+?>
